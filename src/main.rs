ada1f1997d61001ed96694e44855fe21b7b8db16
Marenga <marengamatdrb@gmail.com>
2024-11-13 15:09:24 +0100

    quest xp

--- src/main.rs
+++ src/main.rs
@@ -629,7 +629,7 @@ async fn request(
                 q3.length as ql3,
 
                 activity.id as activityid,
-                c.tavern as tavern_id
+                character.tavern as tavern_id
 
                 FROM character LEFT JOIN activity ON activity.id = \
                      character.activity LEFT JOIN TAVERN on character.tavern \
@@ -686,7 +686,7 @@ async fn request(
                 .query(
                     "UPDATE tavern as t
                  SET tfa = max(0, tfa - ?2)
-                 WHERE a.id = ?1",
+                 WHERE t.id = ?1",
                     params!(tavern_id, quest_length),
                 )
                 .await?;
@@ -749,7 +749,9 @@ async fn request(
                     character.gender,
                     character.class,
 
-                    activity.id as activity_id
+                    activity.id as activity_id,
+
+                    experience
 
                      FROM CHARACTER LEFT JOIN PORTRAIT ON character.portrait = \
                      portrait.id LEFT JOIN tavern on tavern.id = \
@@ -778,14 +780,14 @@ async fn request(
             let subtyp: i32 = row.get(2)?;
 
             let base_index = (7 * (subtyp - 1)) + 3;
-            let (_item, _length, location, monster, mush, silver, xp) = (
+            let (_item, _length, location, monster, mush, silver, quest_xp) = (
                 row.get::<Option<i64>>(base_index)?,
                 row.get::<i64>(base_index + 1)?,
                 row.get::<i64>(base_index + 2)?,
                 row.get::<i64>(base_index + 3)?,
                 row.get::<i64>(base_index + 4)?,
                 row.get::<i64>(base_index + 5)?,
-                row.get::<i64>(base_index + 6)?,
+                row.get::<i32>(base_index + 6)?,
             );
 
             let honor_won = 10;
@@ -796,7 +798,7 @@ async fn request(
             resp.add_val(true as u8); // won
             resp.add_val(0);
             resp.add_val(silver);
-            resp.add_val(xp);
+            resp.add_val(quest_xp);
 
             resp.add_val(mush);
             resp.add_val(honor_won);
@@ -806,7 +808,17 @@ async fn request(
 
             resp.add_key("fightheader.fighters");
             let monster_id = -monster;
-            let player_lvl: i32 = row.get(24)?;
+            let mut player_lvl: i32 = row.get(24)?;
+            let starting_player_xp: i32 = row.get(39)?;
+
+            let mut total_xp = quest_xp + starting_player_xp as i32;
+            let mut required_xp = xp_for_next_level(player_lvl);
+            // Level up the player
+            while total_xp > required_xp {
+                player_lvl += 1;
+                total_xp -= required_xp;
+                required_xp = xp_for_next_level(player_lvl);
+            }
 
             let player_attributes = [1, 1, 1, 1, 1];
             let monster_attributes = [1, 1, 1, 1, 1];
@@ -904,10 +916,13 @@ async fn request(
             let mut res = tx
                 .query(
                     "UPDATE character as c
-                 SET silver = silver + ?2, mushrooms = mushrooms + ?3, honor = \
-                     honor + ?4
+                    SET silver = silver + ?2, mushrooms = mushrooms + ?3, \
+                     honor = honor + ?4, level = ?5, Experience = ?6
                  WHERE c.id = ?1",
-                    params!(player_id, silver, mush, honor_won),
+                    params!(
+                        player_id, silver, mush, honor_won, player_lvl,
+                        total_xp
+                    ),
                 )
                 .await?;
             let row = res.next().await?;
@@ -1032,7 +1047,7 @@ async fn request(
                         return Err(ServerError::BadRequest);
                     }
                     db.query(
-                        "UPDATE character set level = ?1 WHERE id = ?2",
+                        "UPDATE character set level = ?1, experience = 0 WHERE id = ?2",
                         params!(level, player_id),
                     )
                     .await?;
@@ -1593,9 +1608,10 @@ async fn player_poll(
     resp.add_val(1292388336);
     resp.add_val(0);
     resp.add_val(0);
-    resp.add_val(res.get::<i64>(2)?); // Level | Arena << 16
+    let level = res.get::<i32>(2)?;
+    resp.add_val(level); // Level | Arena << 16
     resp.add_val(res.get::<i64>(3)?); // Experience
-    resp.add_val(400); // Next Level XP
+    resp.add_val(xp_for_next_level(level)); // Next Level XP
     let honor = res.get(4)?;
     resp.add_val(honor); // Honor
 
@@ -2429,4 +2445,72 @@ enum Command {
     SetPassword {
         new: String,
     },
+}
+
+fn xp_for_next_level(level: i32) -> i32 {
+    static LOOKUP: [i32; 392] = [
+        400, 900, 1400, 1800, 2200, 2890, 3580, 4405, 5355, 6435, 7515, 8925,
+        10335, 11975, 13715, 15730, 17745, 20250, 22755, 25620, 28660, 32060,
+        35460, 39535, 43610, 48155, 52935, 58260, 63585, 69760, 75935, 82785,
+        89905, 97695, 105485, 114465, 123445, 133260, 143425, 154545, 165665,
+        178210, 190755, 204430, 218540, 233785, 249030, 266140, 283250, 301715,
+        320685, 341170, 361655, 384360, 407065, 431545, 456650, 483530, 510410,
+        540065, 569720, 601435, 633910, 668670, 703430, 741410, 779390, 819970,
+        861400, 905425, 949450, 997485, 1045520, 1096550, 1148600, 1203920,
+        1259240, 1319085, 1378930, 1442480, 1507225, 1575675, 1644125, 1718090,
+        1792055, 1870205, 1949685, 2033720, 2117755, 2208040, 2298325, 2393690,
+        2490600, 2592590, 2694580, 2803985, 2913390, 3028500, 3145390, 3268435,
+        3391480, 3522795, 3654110, 3792255, 3932345, 4079265, 4226185, 4382920,
+        4539655, 4703955, 4870500, 5045205, 5219910, 5405440, 5590970, 5785460,
+        5982490, 6188480, 6394470, 6613125, 6831780, 7060320, 7291640, 7533530,
+        7775420, 8031275, 8287130, 8554570, 8825145, 9107305, 9389465, 9687705,
+        9985945, 10296845, 10611275, 10939230, 11267185, 11612760, 11958335,
+        12318585, 12682650, 13061390, 13440130, 13839160, 14238190, 14653230,
+        15072545, 15508870, 15945195, 16403485, 16861775, 17338505, 17819980,
+        18319895, 18819810, 19344795, 19869780, 20414715, 20964770, 21536005,
+        22107240, 22705735, 23304230, 23925545, 24552535, 25202340, 25852145,
+        26532725, 27213305, 27918540, 28630050, 29367610, 30105170, 30875945,
+        31646720, 32445505, 33251010, 34084530, 34918050, 35789075, 36660100,
+        37561220, 38469755, 39410080, 40350405, 41330960, 42311515, 43326065,
+        44348735, 45405405, 46462075, 47563900, 48665725, 49804020, 50951005,
+        52136360, 53321715, 54555530, 55789345, 57064175, 58348500, 59673840,
+        60999180, 62378435, 63757690, 65180715, 66614100, 68093535, 69572970,
+        71110105, 72647240, 74233350, 75830465, 77476555, 79122645, 80832985,
+        82543325, 84305910, 86080505, 87909870, 89739235, 91636870, 93534505,
+        95490375, 97459260, 99486380, 101513500, 103616290, 105719080,
+        107883715, 110062180, 112305475, 114548770, 116872700, 119196630,
+        121589225, 123996780, 126473000, 128949220, 131514215, 134079210,
+        136717090, 139371155, 142101400, 144831645, 147656105, 150480565,
+        153385655, 156307860, 159310695, 162313530, 165420140, 168526750,
+        171718645, 174929030, 178228565, 181528100, 184937365, 188346630,
+        191849945, 195373130, 198990370, 202607610, 206345275, 210082940,
+        213920015, 217778100, 221739815, 225701530, 229790630, 233879730,
+        238078150, 242299140, 246629445, 250959750, 255429090, 259898430,
+        264482960, 269091720, 273820565, 278549410, 283425105, 288300800,
+        293302740, 298330180, 303483865, 308637550, 313951595, 319265640,
+        324712695, 330187105, 335799860, 341412615, 347193920, 352975225,
+        358901970, 364857940, 370959350, 377060760, 383345695, 389630630,
+        396068325, 402536785, 409164155, 415791525, 422612215, 429432905,
+        436420230, 443440385, 450627180, 457813975, 465210300, 472606625,
+        480177945, 487784290, 495572280, 503360270, 511368340, 519376410,
+        527574890, 535810100, 544235725, 552661350, 561325655, 569989960,
+        578853765, 587756750, 596866840, 605976930, 615337095, 624697260,
+        634274025, 643892430, 653727435, 663562440, 673667980, 683773520,
+        694105920, 704481995, 715093150, 725704305, 736599015, 747493725,
+        758634285, 769821230, 781254025, 792686820, 804425165, 816163510,
+        828158780, 840203305, 852514095, 864824885, 877455675, 890086465,
+        902995095, 915995220, 929193185, 942431150, 956014120, 969597090,
+        983470400, 997398375, 1011626725, 1025885075, 1040443405, 1055031735,
+        1069933505, 1084893105, 1100166150, 1115439195, 1131099560, 1146759925,
+        1162747145, 1178794835, 1195180710, 1211566585, 1228357310, 1245148035,
+        1262290985, 1279497900, 1297057040, 1314616180, 1332609455, 1350602730,
+        1368963280, 1387391470, 1406199095, 1425006720, 1444267210, 1463527700,
+        1483183310,
+    ];
+
+    (level - 1)
+        .try_into()
+        .ok()
+        .and_then(|idx: usize| LOOKUP.get(idx).copied())
+        .unwrap_or(1500000000)
 }
